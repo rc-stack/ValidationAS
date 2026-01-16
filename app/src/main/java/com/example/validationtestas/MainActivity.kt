ca6e280b29206f89ee6a9871178061fb2c07a3b6
rc-stack <rc@before.health>
2026-01-16 12:30:23 -0500

    Small changes

--- app/src/main/java/com/example/validationtestas/MainActivity.kt
+++ app/src/main/java/com/example/validationtestas/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Some data changed $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ValidationTestASTheme {
-        Greeting("Android")
+        Greeting("Android Studio")
     }
 }
